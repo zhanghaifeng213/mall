9c12cf9327483f7329c979325e223799fee596a9
zhanghf <751434992@qq.com>
2018-05-24 14:49:49 +0800

    shopping mall

---
+++ src/view/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">
+    this is id:{{$route.params.testId}}<br>
+    this is name:{{$route.params.testName}}
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
